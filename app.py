bd0e8c1427f84221e9134a975bbb6095d0a9b9ac
jojohi20 <74261595+jojohi20@users.noreply.github.com>
2023-01-13 16:33:28 +0100

    Programm ist besser geworden

--- app.py
+++ app.py
@@ -13,15 +13,11 @@ class Order(db.Model):
     item = db.Column(db.Integer, nullable=False)
     owner = db.Column(db.String(32), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
+    done = db.Column(db.Boolean, default=False)
     time = db.Column(db.DateTime, default=datetime.now)
 
-
-stores = {
-    # "key": "name"
-    "c8753ae80e5093d3293765e93405acb5": "Stand  1",
-    "c8753ae80e5093d3293765e93405acb6": "Stand  2",
-
-}
+    def __repr__(self):
+        return f"Created Order with id: {self.id}"
 
 
 class Product:
@@ -30,62 +26,89 @@ class Product:
         self.id = id
 
 
+class DisplayOrder:
+    def __init__(self, id, amount, itemName, store,displaytime):
+        self.id = id
+        self.amount = amount
+        self.itemName = itemName
+        self.store = store
+        self.displayTime = displaytime
+
+
 products = []
 products.append(Product("Apfel", 1))
 products.append(Product("Banane", 2))
 
+stores = ["Blau", "Rot", "Grün"]
+
+
+def get_product_with_id(id):
+    for p in products:
+        if p.id == id:
+            return p
+
 
 @app.route("/", methods=['GET'])
 def index():
+    #sortiert die Betellungen nach erledigt und unerledigt
     orders = Order.query.order_by(Order.id).all()
-    return render_template("index.html", tasks=orders)
+    displayOrders = []
+
+    for order in orders:
+        if order.done:
+            continue
+
+        itemName = get_product_with_id(order.id).name
+        displayTime = order.time.strftime("%H:%M")
+        displayOrder = DisplayOrder(order.id, order.amount, itemName, order.owner, displayTime)
+        displayOrders.append(displayOrder)
+
+    return render_template("index.html", orders=displayOrders)
 
 
-def createOrder(key, form):
-    print(key)
+def add_order(form, store):
     id = form["id"]
     amount = form["amount"]
-
-    newOrder = Order(item=id, amount=amount, owner=key)
+    newOrder = Order(item=id, amount=amount, owner=store)
     try:
-
         db.session.add(newOrder)
         db.session.commit()
-        print(key)
-        return redirect("/list?key=" + key)
+        print(store)
+        return redirect("/list?store=" + store)
     except:
         return "Failed to create Task"
 
 
 @app.route("/list", methods=['POST', 'GET'])
 def product_list():
-    key = request.args.get("key")
-    print(key)
-    if not key in stores:
-        return "Ungültiger Schlüssel"
+    #mit ?store= werden die Stände identifziert
+    store = request.args.get("store")
+    if not store in stores:
+        return "Ungültiger Stand"
 
     if request.method == 'POST':
-        return createOrder(key, request.form)
+        #fügt die bestellung zur datenbank hinzu
+        return add_order(request.form, store)
 
     else:
-        return render_template("productlist.html", products=products, store=stores[key])
+        return render_template("productlist.html", products=products, store=store)
 
 
-@app.route("/delete/<int:id>")
-def delete(id):
-    del_task = Order.query.get_or_404(id)
+# entfernt die Bestellung aus der Liste
+@app.route("/done/<int:id>")
+def done(id):
+    order = Order.query.get_or_404(id)
 
     try:
-        db.session.delete(del_task)
+        order.done = True
         db.session.commit()
 
     except:
-        print("Error deleting Task")
+        print("Error editing Order")
     return redirect("/")
 
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
     app.run(debug=True)
